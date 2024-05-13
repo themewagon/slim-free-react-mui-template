7fd0d060e962e9c7dab0129b28a7c9e010d2addf
mostofanobi <mntanveer9196@gmail.com>
2024-05-13 12:37:55 +0600

    Updates

--- assets/Login-27e0431c.js
+++
@@ -1,1 +0,0 @@
-import{r,i as a,b as t}from"./index-7991df60.js";var e={},u=a;Object.defineProperty(e,"__esModule",{value:!0});var v=e.default=void 0,i=u(r()),o=t,l=(0,i.default)((0,o.jsx)("path",{d:"M11 7 9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z"}),"Login");v=e.default=l;export{v as d};
